6b7e54f899e5df19dd0c7924b791e3751897de6e
Joy <joymccaffreychicago@gmail.com>
2016-08-29 21:30:07 -0500

    Update images.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 
-10.times do |product|
-  Product.create( :name => Faker::Commerce.product_name, :price => Faker::Commerce.price, :image => Faker::Avatar.image, :description => Faker::Lorem.paragraph )
+Product.all.each do |product|
+  product.update(:image => "http://computer.com/wp-content/uploads/2015/05/HP-Stream-13-Laptop-Includes-Office-365-Personal-for-One-Year-Horizon-Blue-0.jpg" )
 end
