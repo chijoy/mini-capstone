10bee54b9d0ef8ff30808ee909d852548dea6a4e
Joy <joymccaffreychicago@gmail.com>
2016-09-07 21:30:00 -0500

    Add tax and total.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ApplicationRecord
-  # def attributes_list
-  #   attributes.split(', ')
-  # end
+  def discounted?
+    price.to_f < 50
+  end
+
+  def tax
+    price.to_f * 0.09
+  end
+
+  def total
+    price.to_f + tax
+  end
 end
