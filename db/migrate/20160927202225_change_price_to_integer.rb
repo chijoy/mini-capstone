b0b95ea7f90771d978c27d6fe0fce09e15c969e7
Joy <joymccaffreychicago@gmail.com>
2016-09-28 17:09:20 -0500

    Change price to integer.

--- db/migrate/20160927202225_change_price_to_integer.rb
+++
@@ -1,5 +0,0 @@
-class ChangePriceToInteger < ActiveRecord::Migration[5.0]
-  def change
-    change_column :products, :price, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927000441) do
+ActiveRecord::Schema.define(version: 20160928220613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,11 +57,11 @@ ActiveRecord::Schema.define(version: 20160927000441) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.string   "price"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "supplier_id"
+    t.integer  "price"
   end
 
   create_table "suppliers", force: :cascade do |t|
